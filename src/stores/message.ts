46c9850df75a54816f371b42336841d512e52433
liupenghui <liupenghui@58.com>
2025-09-11 11:31:05 +0800

    review

--- src/stores/message.ts
+++ src/stores/message.ts
@@ -1,8 +1,8 @@
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
 
-export const useMessageStore = defineStore('message', {
+export const useMessageStore = defineStore("message", {
   state: () => ({
-    message: 'Hello World',
+    message: "Hello World",
   }),
   getters: {
     fullMessage: (state) => `The message is "${state.message}".`,
@@ -13,16 +13,16 @@ export const useMessageStore = defineStore('message', {
       return new Promise((resolve) => {
         setTimeout(() => {
           // 这里的 this 是当前的 Store 实例
-          this.message = newMessage
-          resolve('Async done.')
-        }, 3000)
-      })
+          this.message = newMessage;
+          resolve("Async done.");
+        }, 3000);
+      });
     },
     // 同步更新 message
     updateMessageSync(newMessage: string): string {
       // 这里的 this 是当前的 Store 实例
-      this.message = newMessage
-      return 'Sync done.'
+      this.message = newMessage;
+      return "Sync done.";
     },
   },
-})
+});
